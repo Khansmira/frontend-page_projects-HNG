875beef43416d1c0b74c1788b95f05852f9c4510
Khansmira <miriamakanwa@gmail.com>
2023-09-11 09:50:44 -0700

    files:update all

---
+++ README.md
@@ -0,0 +1,1 @@
+#HNG frontend pages task
---
+++ script.js
@@ -0,0 +1,29 @@
+function updateCurrentDay() {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const currentDay = daysOfWeek[currentDate.getUTCDay()];
+  const currentDayElement = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+  currentDayElement.textContent = `Current day of the week: ${currentDay}`;
+}
+
+function updateCurrentUTCTime() {
+  const currentDate = new Date();
+  const currentUTCTimeMilliseconds = currentDate.getTime(); // Get time in milliseconds
+  const currentUTCTimeElement = document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  );
+  currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTimeMilliseconds}`;
+}
+
+updateCurrentDay();
+updateCurrentUTCTime();
